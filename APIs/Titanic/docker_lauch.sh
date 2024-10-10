25f10e967cc59394a4686d302782510f4680cd51
meiyor <juan.mayortorres@ex-staff.unitn.it>
2024-10-10 12:12:06 -0500

    update new files and remove olds

--- APIs/Titanic/docker_lauch.sh
+++ APIs/Titanic/docker_lauch.sh
@@ -1,5 +1,5 @@
 docker-compose -f "docker-compose.yml" up -d --build
-docker-compose up
+(docker-compose up; cp -r ./mount/results/ ./results/; cp -r  ./mount/postgres/ /var/lib/postgresql/16/main/)
 
 # use this if you want to run it directly from the Dockerfile
 # docker run -t --name titanic-api-2x titanic_frontend

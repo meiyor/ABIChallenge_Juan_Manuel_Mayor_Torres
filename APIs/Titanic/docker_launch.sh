f4fa6f132f81419e76c7a0cf985235f307beb4d9
meiyor <juan.mayortorres@ex-staff.unitn.it>
2024-10-11 01:46:42 -0500

    new files adding

--- APIs/Titanic/docker_launch.sh
+++ APIs/Titanic/docker_launch.sh
@@ -1,6 +1,8 @@
 # cp -r /var/lib/postgresql/16/main/* ./mounts/postgres/
-docker-compose -f "docker-compose.yml" up -d --build
-(docker-compose up; cp -r ./mounts/postgres/ /var/lib/postgresql/16/main/)
+# docker-compose -f "docker-compose.yml" up -d --build
+docker-compose up;
+
+#cp -r ./mounts/postgres/ /var/lib/postgresql/16/main/)
 
 # use this if you want to run it directly from the Dockerfile
 # docker run -t --name titanic-api-2x titanic_frontend
